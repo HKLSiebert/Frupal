a120e863354baa644d69465dc1a3d9fb437d7701
Taishawn Fontleroy <taishawn@ada.cs.pdx.edu>
2020-11-30 17:16:33 -0800

    Updated my status constructor

--- status.cpp
+++ status.cpp
@@ -1,40 +1,67 @@
 #include "status.h"
-#include "displayClass.h"
 
 
-status::status()
+status::status(frupalMap & startmap)
 {
-  startx,starty = 0;
+  //startx,starty = 0;
+  map=new object*[/*size of map*/];
+  map = new object*[/* size of map*/];
+  loadmap(startmap);
 }
-
-void status::updatemap(int x,int y)
+bool status::update(char userinput)
 {
-  Display display_map;
-  //hero coords
-  startx=x;
-  starty=y;
-  display_map.UpdatePlayerPosition(startx,starty);
-  //figure out how interacting is working
+  switch(getch())
+  {
+    case KEY_LEFT:
+      if(my_hero.interact(map[x-1][y]))
+        --x;
+      break;
+    case KEY_RIGHT:
+      my_hero.interact(map[x+1][y]);
+      break;
+    case KEY_UP:
+      my_hero.interact(map[x][y+1]);
+      break;
+    case KEY_DOWN:
+      my_hero.interact(map[x][y-1]);
+      break;
+    case 'x':
+    case 'X':
+      loop_again = false; // Exit Frupal
+      break;
+  }
+  void status::updatemap(int x,int y)
+  {
+    //hero coords
+    startx=x;
+    starty=y;
+    //display_map.UpdatePlayerPosition(startx,starty);
+    //figure out how interacting is working
 
-}
+  }
 
-void status::loadmap(char ** map)
-{
-  //need to load map so i can access and change whatever is necessary 
-}
+  void status::loadmap(frupalMap & startmap)
+  {
+    //need to load map so i can access and change whatever is necessary 
+  }
 
 
-int status::gameprogress()
-{
-  //if(my_hero->hero_health() < 0)
-  //return -1 //lost the game
-  // if(my_hero->check for game ending jewel)//bool return value
-  // return 1 //won the game
-  return 0;//game still going
-}
+  int status::gameprogress()
+  {
+    //if(my_hero->hero_health() < 0)
+    //return -1 //lost the game
+    // if(my_hero->check for game ending jewel)//bool return value
+    // return 1 //won the game
+    return 0;//game still going
+  }
 
-//I was thinking maybe draw passes in what index they need then i return the list ptr which has everything that they need
-list* status::draw_update(int x,int y)
-{
-  return map_array[x][y].list;
-}
+  //I was thinking maybe draw passes in what index they need then i return the list ptr which has everything that they need
+  list* status::draw_display(int x,int y)
+  {
+    //map[x][y]->getdisplaychar(); 
+    //return map_array[x][y].list;
+  }
+
+
+
+
--- status.h
+++ status.h
@@ -1,5 +1,8 @@
 #include <string>
 #include <iostream>
+#include "frupalMap.h"
+//#include "displayClass.h"
+#include "object.h"
 using namespace std;
 
 struct list
@@ -11,23 +14,25 @@ struct list
   color background_color;
   bool visible;
 };
+
 //Need to remember if hero has binoculars have to update how many sqaures he can look at.
 class status
 {
   public:
-    status();
+    status(frupalMap & startmap);
     void update_map(int x, int y/* user input*/);
-    void load_map(char **);//loads up the mapp array to update it;
+    void load_map(frupalMap & startmap);//loads up the mapp array to update it;
     bool game_progress();//is user did or did they complete the game?
     int success();
     void cursor(/*dont exactly know what im doing for the cursor yet*/);
-    list* draw_update(int x,int y);//function to pass the updated index to draw
+    list* draw_display(int x,int y);//function to pass the updated index to draw
+    bool update(char userinput);
   protected:
     int startx;//starting coords of hero
     int starty;
     int mapx;
     int mapy;
-    object * items;
+    object ** map;
     hero * my_hero;
-    char ** map_array; // However we are handling the data structure of the map.
+    list ** map_display; // However we are handling the data structure of the map.
 };
